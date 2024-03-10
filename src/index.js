406cd1ad0619c5731cbe42692b9069bfca8c898a
Nikhil Chaudhari <nikhil-3112@github.com>
2024-03-10 20:20:35 +0530

    commit1

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import { getAuth } from "firebase/auth";
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
     apiKey: "AIzaSyDFOvn4Wnvr79Gt4Svo5_1IibAnLTbgouU",
-    authDomain: "educonnect-dbdeb.firebaseapp.com",
-    projectId: "educonnect-dbdeb",
-    storageBucket: "educonnect-dbdeb.appspot.com",
+    authDomain: "EduNeat-dbdeb.firebaseapp.com",
+    projectId: "EduNeat-dbdeb",
+    storageBucket: "EduNeat-dbdeb.appspot.com",
     messagingSenderId: "228460765976",
     appId: "1:228460765976:web:9e7b1c58c75f51045f346d",
     measurementId: "G-96FV7MP657"
